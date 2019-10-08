df236f2f751df294dc303e1f3b36734b512e98f6
Kimihiro N <kimihiro.nisimura@gmail.com>
2019-10-08 09:44:04 +0900

    :bug: 動くように修正

--- src/index.js
+++ src/index.js
@@ -1,43 +1,39 @@
-var http = require('http');
+const http = require('http');
     url = require('url'),
     fs = require('fs'),
     jquery = require('jquery'),
     jsdom = require('jsdom'),
     jCanvas = require('jcanvas'),
     unique_ogp = require('./unique_ogp.js');
+const { JSDOM } = jsdom;
 
-
-var getArticon = function(title, brand, callback) {
-    jsdom.env("", function (errors, window) {
-      var $ = jquery(window);
-      jCanvas( $, window);  
-      var canvas = $("<canvas />");
-      canvas[0].width = 1200;
-      canvas[0].height = 630;
-      canvas = unique_ogp.draw(canvas, title, brand);
-      callback(canvas.get(0));
-    });
+var getArticon = function(title, brand) {
+    const { window } = new JSDOM(`<!DOCTYPE html><canvas id="canvas" width="1200" height="630"></canvas>`, {runScripts: "outside-only"});
+    const $ = jquery(window);
+    jCanvas($, window);
+    var canvas = $("#canvas");
+    canvas = unique_ogp.draw(canvas, title, brand);
+    return canvas[0];
 };
 
 const port = process.env.PORT || 8088;
 
+function atob(a) {
+    return Buffer.from(a, 'base64')
+}
 // create server
 http.createServer(function(request, response) {
-    query = url.parse(request.url, true).query;
-    title = query["title"];
-    brand = query["brand"];
+    let query = url.parse(request.url, true).query;
+    let title = query["title"];
+    let brand = query["brand"];
     if (!title) {
         response.write("Add Query to this page. '?title=$TITLE&brand=$BRAND'");
         return response.end();
     }
-    getArticon(title, brand, function(result) {
-        stream = result.pngStream();
-        response.writeHead(200, {"Content-Type": "image/png"});
-        stream.on("data", function(d) {
-            response.write(d);
-        });
-        stream.on("end", function() {
-            response.end();
-        });
-    });    
+    let canvas = getArticon(title, brand);
+    const image = atob(canvas.toDataURL('image/png').split(',')[1]);
+    response.writeHead(200, {"Content-Type": "image/png"});
+    response.write(image);
+    response.end();
+
 }).listen(port, '0.0.0.0');
